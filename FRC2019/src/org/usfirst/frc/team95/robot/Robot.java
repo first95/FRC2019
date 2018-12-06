399fa02c6bb62c552ad621618a56bc77efaf6657
PJG3000\Philip <philip@sugargroup.net>
2018-12-05 20:26:23 -0500

    This is basically everything found in the EncoderFollower class for the path finder, plus the trajectory generator that was put into Robot.java. all of the doubles and ints declared after the trajectory generator were made to fix errors. I have no idea if that was the correct thing to do.

--- FRC2019/src/org/usfirst/frc/team95/robot/Robot.java
+++ FRC2019/src/org/usfirst/frc/team95/robot/Robot.java
@@ -15,6 +15,7 @@ import org.usfirst.frc.team95.robot.subsystems.DriveBase;
 import jaci.pathfinder.Pathfinder;
 import jaci.pathfinder.Trajectory;
 import jaci.pathfinder.Waypoint;
+import jaci.pathfinder.modifiers.TankModifier;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -60,16 +61,6 @@ public class Robot extends IterativeRobot {
 	 */
 	@Override
 	public void robotInit() {
-
-		Trajectory.Config config = new Trajectory.Config(Trajectory.FitMethod.HERMITE_CUBIC, Trajectory.Config.SAMPLES_HIGH, 0.05, 1.7, 2.0, 60.0);
-		Waypoint[] points = new Waypoint[] {
-		        new Waypoint(-4, -1, Pathfinder.d2r(-45)),
-		        new Waypoint(-2, -2, 0),
-		        new Waypoint(0, 0, 0)
-		};
-		
-		Trajectory trajectory = Pathfinder.generate(points, config);
-		double varr = trajectory.segments[0].velocity;
 
 		// Initialize all subsystems
 		drivebase = new DriveBase();
