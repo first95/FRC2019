17d2b54abb1270fa79189760665a5e75a690c346
John Walthour <john.walthour@gmail.com>
2019-01-12 15:30:13 -0500

    UPdates to imports to compile

--- VisionCoprocessor/src/main/java/GripPipelineLinesFromTarget.java
+++ VisionCoprocessor/src/main/java/GripPipelineLinesFromTarget.java
@@ -1,20 +1,14 @@
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
-import java.util.HashMap;
 
-import edu.wpi.first.wpilibj.vision.VisionPipeline;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.imgproc.LineSegmentDetector;
 
-import org.opencv.core.*;
-import org.opencv.core.Core.*;
-import org.opencv.features2d.FeatureDetector;
-import org.opencv.imgcodecs.Imgcodecs;
-import org.opencv.imgproc.*;
-import org.opencv.objdetect.*;
+import edu.wpi.first.vision.VisionPipeline;
 
 /**
 * GripPipelineLinesFromTarget class.
