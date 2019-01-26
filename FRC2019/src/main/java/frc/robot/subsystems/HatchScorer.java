a5d7d8c5b8e1b2533089c794cb88e657d18d1e89
Shared02 <lindsayvallen@gmail.com>
2019-01-26 16:43:18 -0500

    Adding stub of hatch scorer in fulfillment of git issue #14. Built code, but haven't deployed it yet, so probably doesn't work as intended.

---
+++ FRC2019/src/main/java/frc/robot/subsystems/HatchScorer.java
@@ -0,0 +1,48 @@
+package frc.robot.subsystems;
+
+import frc.robot.Constants;
+import frc.robot.components.SolenoidI;
+import frc.robot.components.SolenoidWrapper;
+import frc.robot.commands.LoadHatch;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ * Controls mechanism to score hatches.
+ */
+public class HatchScorer extends Subsystem {
+
+  // The solenoids for the cylinders that open the scorer and that push the scorer
+  // Named to allow for future possibility of adding one more solenoid per motion to have slow/fast options
+	private SolenoidI openA, pushA;
+
+  public HatchScorer() {
+    // False means closed and true means open so default is closed when robot off
+    openA = new SolenoidWrapper(Constants.HS_OPEN_A);
+
+    // 00 means ?; 01 means ?; 10 means ?; and 11 means?
+    pushA = new SolenoidWrapper(Constants.HS_PUSH_A);
+
+  }
+
+  @Override
+  public void initDefaultCommand() {
+    setDefaultCommand(new LoadHatch());
+  }
+
+  public void openHS(boolean open) {
+    if (open) {
+      openA.set(true);
+    } else {
+      openA.set(false);
+    }
+  }
+
+  public void pushHS(boolean push) {
+    if (push) {
+      pushA.set(true);
+    } else {
+      pushA.set(false);
+    }
+  }
+}
