a28b538c50ab6196cb7aadca05d89073637bc7f0
John Walthour <john.walthour@gmail.com>
2019-01-26 14:41:54 -0500

    Added stubs for vision coprocessor subsystem and a setter command

---
+++ FRC2019/src/main/java/frc/robot/subsystems/VisionCoprocessor.java
@@ -0,0 +1,44 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import java.util.LinkedList;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.commands.SetCameraMode;
+
+/**
+ * A subsystem to read from the vision coprocessor
+ */
+public class VisionCoprocessor extends Subsystem {
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    @Override
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        setDefaultCommand(new SetCameraMode());
+    }
+
+    public class VisionTargetInfo {
+        public double bearingDegrees;
+        public double rangeInches;
+    }
+
+    public LinkedList<VisionTargetInfo> getCurVisibleVisionTargets() {
+        return new LinkedList<VisionTargetInfo>();
+    }
+
+    public boolean isCameraHumanVision() {
+        return false;
+    }
+
+    public void setCameraIsHumanVisible() {
+
+    }
+}
