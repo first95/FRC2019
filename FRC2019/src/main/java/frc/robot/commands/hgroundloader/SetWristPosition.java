3a42169291c0e104960b1ef4261cf211c1ac4ae3
John Walthour <john.walthour@gmail.com>
2019-02-12 17:59:20 -0500

    +option to finish immediately

--- FRC2019/src/main/java/frc/robot/commands/hgroundloader/SetWristPosition.java
+++ FRC2019/src/main/java/frc/robot/commands/hgroundloader/SetWristPosition.java
@@ -4,11 +4,18 @@ import frc.robot.Robot;
 import edu.wpi.first.wpilibj.command.Command;
 
 public class SetWristPosition extends Command {
-	private double wristPosition;
-	
-	public SetWristPosition(double position) {
+    private double wristPosition;
+    private boolean waitForWristToReachTarget;
+    
+    /**
+     * 
+     * @param position the target position to seek
+     * @param wait when true, this command waits until the wrist reaches the target before reporting completion.  When false, this command sets the setpoint and then ends immediately.
+     */
+	public SetWristPosition(double position, boolean wait) {
         requires(Robot.hGroundLoader);
         wristPosition = position;
+        waitForWristToReachTarget = wait;
 	}
 
 	@Override
@@ -18,7 +25,7 @@ public class SetWristPosition extends Command {
 	
 	@Override
 	protected boolean isFinished() {
-		return Robot.hGroundLoader.isWristPositionOnTarget();
+		return (!waitForWristToReachTarget) || Robot.hGroundLoader.isWristPositionOnTarget();
 	}
 
 }
