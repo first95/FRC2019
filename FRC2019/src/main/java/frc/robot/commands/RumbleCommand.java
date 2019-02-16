4a5732c5b574c24a916d42df86526f8eb2e28cf1
John Walthour <john.walthour@gmail.com>
2019-02-16 14:12:50 -0500

    Removed empty functions

--- FRC2019/src/main/java/frc/robot/commands/RumbleCommand.java
+++ FRC2019/src/main/java/frc/robot/commands/RumbleCommand.java
@@ -47,25 +47,9 @@ public class RumbleCommand extends Command {
         endTime = Timer.getFPGATimestamp() + duration;
     }
 
-    // Called repeatedly when this Command is scheduled to run
-    @Override
-    protected void execute() {
-    }
-
     // Make this return true when this Command no longer needs to run execute()
     @Override
     protected boolean isFinished() {
         return finishImmediately || (Timer.getFPGATimestamp() > endTime);
-    }
-
-    // Called once after isFinished returns true
-    @Override
-    protected void end() {
-    }
-
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    @Override
-    protected void interrupted() {
     }
 }
