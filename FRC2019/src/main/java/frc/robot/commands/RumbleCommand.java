23cc93c0507944d503a32caf780d36e5d50bf800
John Walthour <john.walthour@gmail.com>
2019-02-19 19:31:16 -0500

    Simplified

--- FRC2019/src/main/java/frc/robot/commands/RumbleCommand.java
+++ FRC2019/src/main/java/frc/robot/commands/RumbleCommand.java
@@ -7,22 +7,18 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.TimedCommand;
 import frc.robot.OI;
 import frc.robot.Robot;
 
 /**
  * Rumble for a given amount of time
  */
-public class RumbleCommand extends Command {
+public class RumbleCommand extends TimedCommand {
     private OI.Controller controller;
     private OI.RumbleType pitch;
     private double severity;
     private double duration;
-    private boolean finishImmediately;
-
-    private double endTime;
 
     /**
      * Rumble one of the controllers
@@ -33,11 +29,11 @@ public class RumbleCommand extends Command {
      * @param finishImmediately true to start the controller rumbling and immediately move on to the next command.  false to wait until the rumble duration is over before moving onto the next command.
      */
     public RumbleCommand(OI.Controller controller, OI.RumbleType pitch, double severity, double duration, boolean finishImmediately) {
+        super(finishImmediately? 0.0001 : duration);
         this.controller = controller;
         this.pitch = pitch;
         this.severity = severity;
         this.duration = duration;
-        this.finishImmediately = finishImmediately;
     }
 
     // Called just before this Command runs the first time
@@ -45,12 +41,5 @@ public class RumbleCommand extends Command {
     public void start() {
         super.start();
         Robot.oi.Rumble(controller, pitch, severity, duration); 
-        endTime = Timer.getFPGATimestamp() + duration;
-    }
-
-    // Make this return true when this Command no longer needs to run execute()
-    @Override
-    protected boolean isFinished() {
-        return finishImmediately || (Timer.getFPGATimestamp() > endTime);
     }
 }
