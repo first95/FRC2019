05587939a7074d2d9295760f006ddce06cbb9f23
John Walthour <john.walthour@gmail.com>
2019-03-01 13:06:12 -0500

    Removed delays

--- FRC2019/src/main/java/frc/robot/commands/hgroundloader/AutoAcquire.java
+++ FRC2019/src/main/java/frc/robot/commands/hgroundloader/AutoAcquire.java
@@ -19,13 +19,10 @@ public class AutoAcquire extends CommandGroup {
     public AutoAcquire(boolean buzz) {
         super();
         // Drop it
-        addSequential(new RumbleCommand(Controller.DRIVER, RumbleType.HIGH_PITCH  ,1.0 , RUMBLE_TIME_S, false));
         addSequential(new SetWristAngle(HatchGroundLoader.COLLECT_DEG, true));
         addSequential(new RumbleCommand(Controller.DRIVER, RumbleType.HIGH_PITCH, 1.0, RUMBLE_TIME_S, false));
         // Spin it 
         addSequential(new SetIntakeThrottle(AUTO_ACQUIRE_INTAKE_THROTTLE));        
-        // Wait for it
-        addSequential(new Pause(SPINUP_DURATION_S));
         // Waaaaaaaait for it
         addSequential(new WaitForHatchDetected());
         // Stop it
