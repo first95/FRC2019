ae217ece7e2a6064076259a58e74b8fe923a05b9
John Walthour <john.walthour@gmail.com>
2019-04-05 20:27:00 -0400

    Both sides rumbled here

--- FRC2019/src/main/java/frc/robot/commands/AnnouncingGroup.java
+++ FRC2019/src/main/java/frc/robot/commands/AnnouncingGroup.java
@@ -8,7 +8,7 @@ public class AnnouncingGroup extends CommandGroup {
 	public AnnouncingGroup() {  
         super();
 
-        addSequential(new RumbleCommand(OI.Controller.DRIVER, Joystick.RumbleType.kLeftRumble, 1.0, 1.0, true));
+        addSequential(new RumbleCommand(OI.Controller.DRIVER, Joystick.RumbleType.kLeftRumble, 1.0, 1.0, false));
         addSequential(new RumbleCommand(OI.Controller.DRIVER, Joystick.RumbleType.kRightRumble, 1.0, 1.0, true));
         addSequential(new Nothing());
 	}
